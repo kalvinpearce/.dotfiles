c49f7a5cf4bbed0f76b02d62d84a881b7057abf7
Kalvin Pearce <kalvinpearce@gmail.com>
2022-04-11 22:14:18 +1000

    Fix cargo env error

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,5 +7,4 @@ export HISTFILE="$ZDOTDIR/.zhistory"    # History filepath
 export HISTSIZE=10000                   # Maximum events for internal history
 export SAVEHIST=10000                   # Maximum events in history file
 
-source "$HOME/.cargo/env"
 export PATH="$HOME/.cargo/bin:$PATH"
